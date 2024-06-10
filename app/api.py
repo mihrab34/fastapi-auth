99de1bdcbfd15f03a06b72a5b9931f2805375300
Akintola Rahmat <akintolarahmah@gmail.com>
2024-06-10 14:40:22 +0000

    added routes and model for rants

--- app/api.py
+++ app/api.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from app.model import RantSchema
 
+rants = [
+    {
+        "id": 1,
+        "content": "Hmmmm..... i don't know what to rant yet"
+    }
+]
+
+users = []
+
 app = FastAPI()
 
 
@@ -9,9 +18,19 @@ async def read_root() -> dict:
     return {"message": "Welcome to my rant page!"}
 
 
-rants = [
-    {
-        "id": 1,
-        "content": "This is a rant secured with pyJWT and Pydantic",
-    }
-]
+@app.get("/rants", tags=["rants"])
+async def get_rants() -> dict:
+    return {"data": rants}
+
+
+@app.get("/rants/{id}", tags=["rants"])
+async def get_single_rant(id: int) -> dict:
+    if id > len(rants):
+        return {
+            "error": "Rant not found with supplied ID"
+        }
+    for rant in rants:
+        if rant["id"] == id:
+            return {
+                "data": rant
+            }
