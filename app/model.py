226ce5d3333b790d3cf2fe8038ba078a9049cea5
Akintola Rahmat <akintolarahmah@gmail.com>
2024-06-13 11:18:30 +0000

    add user model

--- app/model.py
+++ app/model.py
@@ -12,3 +12,31 @@ class RantSchema(BaseModel):
                 "content": "This is a rant page secured with pyJWT and Pydantic"
             }
         }
+
+
+class UserSchema(BaseModel):
+    username: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "username": "johndoe",
+                "email": "johndoe@me.com",
+                "password": "secret"
+            }
+        }
+
+
+class UserLoginSchema(BaseModel):
+    email: str = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "johndoe@me.com",
+                "password": "secret"
+            }
+        }
